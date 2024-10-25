4aa1007ef9aec4a883673c42a58c1813e3ee140a
Abhinav Kr. Singh <abhinavkr.singh@Abhinavs-MacBook-Air.local>
2024-10-25 21:48:00 +0530

    added rate limiting to api

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import apiRoutes from './src/api';  
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
-app.use(cors());
 const port = 4000;
 
-app.use('/api', apiRoutes); 
+app.use(cors());
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 10, 
+    message: "Too many requests from this IP, please try again later."
+});
+
+app.use('/api',apiLimiter, apiRoutes); 
 
 app.get('/', (req, res) => {
     res.send('Welcome to Cricket Match API');
